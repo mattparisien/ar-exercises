f6f44c3477de8752965f638752330e3ded9dbc42
Matthew Parisien <matthewparisien4@gmail.com>
2022-01-05 15:50:31 +0000

    Destroy store3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,10 +6,16 @@ puts "----------"
 
 # Your code goes here ...
 
+#Assign store1 & store2 to instance vars
 @store1 = Store.find_by(id: 1);
 @store2 = Store.find_by(id: 2);
+@store3 = Store.find_by(id: 3);
 
+#Update store's name
 @store1.name = 'Montreal'
+
+#Delete store3
+@store3.destroy
 
 puts @store1.name
 
