373161db63c7b1a401bf63147bcb29f0df5e0756
Matthew Parisien <matthewparisien4@gmail.com>
2022-01-05 16:33:54 +0000

    Count stores with a revenue of over a million

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -10,5 +10,10 @@ puts "----------"
 # Your code goes here ...
 
 @total_revenue = Store.sum(:annual_revenue)
+@average_annual_revenue = Store.average(:annual_revenue)
+@count_revenue_over_a_million = Store.where("annual_revenue > ?", 1000000).count
 
-puts @total_revenue
+
+puts "The total revenue is #{@total_revenue}"
+puts "The average annual revenue is #{@average_annual_revenue}"
+puts "The number of stores with a revenue over a million is #{@count_revenue_over_a_million}"
