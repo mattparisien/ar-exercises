898e05b83cb95b01e3a39ff7cf8af2bd28c8ea77
Matt Parisien <matthewparisien4@gmail.com>
2022-01-05 12:02:32 -0500

    Implement employee validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+#Employee validations
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than: 40 }
+  validates :hourly_rate, numericality: { less_than: 200 }
+  validates :store_id, presence: true
+end
