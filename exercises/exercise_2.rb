008451be1a532ad4db4bf38dd0215430e5712858
Matthew Parisien <matthewparisien4@gmail.com>
2022-01-05 15:38:11 +0000

    Update store2's name in ex 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1);
+@store2 = Store.find_by(id: 2);
+
+@store1.name = 'Montreal'
+
+puts @store1.name
+
+
+
+
